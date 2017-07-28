eaebb83b207559c5efe877c65d355e6002ee89a4
rclaire15 <clairecardino15@gmail.com>
2017-07-28 13:58:48 +0800

    to heroku:

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
+/require modules
 var express = require('express');
 var path = require('path');
-
+//instantiate express
 var app = express();
-
+//set port
 app.set('port', (process.env.PORT || 5000));
-
+//use static files
 app.use(express.static(path.join(__dirname, 'public')));
-
+//express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/index1.html'));
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index1.html'));
 });
-
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/index3.html'));
+app.get('/json', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index2.html'));
 });
-
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'public/lists.htm'));
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, 'views/index3.html'));
+});
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
 });
 
 var server = app.listen(app.get('port'), function(){
